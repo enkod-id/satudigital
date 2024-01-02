3b9fae714154717e616067750c29796d0a4f4388
enkod-id <natanaeltamto@gmail.com>
2024-01-02 09:40:04 +0700

    myproject digital

--- week17/fe/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as User } from './User';
-export { default as Navbar } from './Navbar';
-export { default as ProductList } from './ProductList';
-export { default as ProductForm } from './ProductForm';
-export { default as LoginForm } from './LoginForm';
--- week17/fe/src/layouts/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as PublicLayout } from './PublicLayout';
-export { default as AnotherLayout } from './AnotherLayout';
